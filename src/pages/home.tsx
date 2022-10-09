96d33e10b92917ccc3d2789e16e0dbc0c2da1a73
linmumu <1054742101@qq.com>
2022-10-09 14:00:43 +0800

    完成Chart12

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -11,6 +11,8 @@ import {Chart7} from '../components/Chart-7';
 import {Chart8} from '../components/Chart-8';
 import {Chart9} from '../components/Chart-9';
 import {Chart10} from '../components/Chart-10';
+import {Chart11} from '../components/Chart-11';
+import {Chart12} from '../components/Chart-12';
 
 export const Home = () => {
 
@@ -46,12 +48,13 @@ export const Home = () => {
 						<h2>案发类型统计</h2>
 						<div className="charts">
 							<Chart10/>
+							<Chart11/>
 						</div>
 					</div>
 					<div className="bordered row2 案发街道">
 						<h2>案发街道统计</h2>
 						<div className="charts">
-
+							<Chart12/>
 						</div>
 					</div>
 					<div className="bordered row3 作案手段">
