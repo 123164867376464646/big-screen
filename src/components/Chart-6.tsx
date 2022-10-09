d6ad8601b48b58f5bd326538406878c4cbe866a9
linmumu <1054742101@qq.com>
2022-10-09 12:32:39 +0800

    修改 Chart6 配置样式

--- src/components/Chart-6.tsx
+++ src/components/Chart-6.tsx
@@ -2,88 +2,87 @@ import React, {useEffect, useRef} from "react";
 import * as echarts from 'echarts';
 import china from '../geo/china.json';
 import {createEchartsOptions} from '../shared/create-echarts-options';
+import AreaArray from '../shared/area';
+import {px} from '../shared/px';
 
 export const Chart6 = () => {
 	const divRef = useRef(null);
-	const colors = {'青海省': '#BB31F7', '甘肃省': '#15B8FD', '四川省': '#06E1EE', '新疆维吾尔自治区': '#dd3c2d'};
-
 	useEffect(() => {
 		const myChart = echarts.init(divRef.current);
-
 		// @ts-ignore
 		echarts.registerMap('CN', china);
-		myChart.setOption(createEchartsOptions({
+		let chartOption = {
+			tooltip: {
+				trigger: 'item',
+				triggerOn: 'mousemove|click',
+				showDelay: 0,
+				alwaysShowContent: true,
+				position: [px(10), px(10)],
+				backgroundColor: '#0f113a',
+				borderColor: '#6879e4',
+				borderWidth: px(2),
+				color: 'white',
+				textStyle: {
+					color: 'white'
+				},
+				transitionDuration: 0.2
+			},
 			xAxis: {show: false},
 			yAxis: {show: false},
+			visualMap: {
+				left: 'right',
+				bottom: '8%',
+				min: 0,
+				max: 34,
+				inRange: {
+					color: [
+						'rgba(109,127,238,1)',
+						'rgba(109,127,238,0.8)',
+						'rgba(109,127,238,0.6)',
+						'rgba(109,127,238,0.5)',
+						'rgba(109,127,238,0.4)',
+						'rgba(109,127,238,0.3)',
+						'rgba(109,127,238,0.2)',
+						'rgba(109,127,238,0.1)',
+						'rgba(109,127,238,0.05)',
+					]
+				},
+				text: ['高', '低'],
+				textStyle: {
+					color: 'white',
+					fontSize: px(16)
+				},
+				calculable: true
+			},
 			series: [
 				{
+					name: '分布情况',
 					type: 'map',
-					mapType: 'CN', // 自定义扩展图表类型
-					data: [
-						{name: '甘肃省', value: 1},
-					],
+					mapType: 'CN',
+					data: AreaArray(),
 					label: {show: false, color: 'white'},
 					itemStyle: {
-						shadowColor: '#002b62',
-						shadowBlur: 2,
+						shadowColor: '#01091f',
+						shadowBlur: px(2),
+						color: 'rgba(255,255,255,0)',
 						areaColor: '#010D3D',
-						color: colors['甘肃省'],
-						borderColor: '#01A7F7',
+						borderColor: '#2891BD',
 						emphasis: {
-							label: {color: 'white'},
-							areaColor: '#5470C6',
+							label: {show: true, color: 'white'},
+							areaColor: '#010D3D',
 						},
 					}
 				},
-				{
-					type: 'map',
-					mapType: 'CN', // 自定义扩展图表类型
-					data: [
-						{name: '四川省', value: 100},
-					],
-					itemStyle: {
-						areaColor: '#010D3D',
-						color: colors['四川省'],
-						borderColor: 'yellow',
-						emphasis: {
-							label: {color: 'white'},
-							areaColor: '#5470C6',
-						},
-					}
-				},
-				{
-					type: 'map',
-					mapType: 'CN', // 自定义扩展图表类型
-					data: [
-						{name: '青海省', value: 100},
-					],
-					itemStyle: {
-						areaColor: '#010D3D',
-						color: colors['青海省'],
-						borderColor: '#01A7F7',
-						emphasis: {
-							label: {color: 'white'},
-							areaColor: '#ffffff',
-						},
-					}
-				},
-
 			]
-		}));
+		};
+		myChart.setOption(createEchartsOptions(chartOption));
 	}, []);
 
 	return (
 		<div className="bordered 分布地">
-			<h2>全重庆市犯罪人员籍贯分布地</h2>
-			<div className="wrapper">
-				<div ref={divRef} className="chart"/>
-				<div className="legend bordered">
-					<span className="icon" style={{background: colors['甘肃省']}}/>甘肃籍
-					<span className="icon" style={{background: colors['四川省']}}/>四川籍
-					<span className="icon" style={{background: colors['青海省']}}/>青海籍
-				</div>
-				<div className="notes">此地图仅显示了中国的部分区域</div>
-			</div>
+			<h2>全市犯罪人员籍贯分布地</h2>
+			<div ref={divRef} className="chart"></div>
+			<div className="notes">此地图仅展示部分地区</div>
 		</div>
 	);
 };
