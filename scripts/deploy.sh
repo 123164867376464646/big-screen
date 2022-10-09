3c85c971390c93147befa574c57b82f3a510ac59
linmumu <1054742101@qq.com>
2022-10-10 01:03:51 +0800

    添加 scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m deploy &&
-git remote add origin git@github.com:FrankFang/Se1uqKnmKDZS.git &&
-git push -uf origin master:gh-pages &&
-cd -;
+git remote add origin git@gitee.com:linmumujiayou/big-screen-website.git &&
+git push -u -f origin master &&
+cd ..;
